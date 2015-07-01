9dc1a1c179fbe2150b0a1623b2d7cef130d186b1
valentin <vasilescu.valent@gmail.com>
2015-07-01 18:15:38 +0300

    intial

--- app/src/main/java/com/example/valentin/myapplication/JsonStats.java
+++ app/src/main/java/com/example/valentin/myapplication/JsonStats.java
@@ -3,7 +3,6 @@ package com.example.valentin.myapplication;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
